be257784d117ae61f9dbeb19b40f4363ef10c9b5
Your Name <you@example.com>
2023-04-10 14:54:30 +0200

    feat: implement two different button styles

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,8 +1,19 @@
 import IconicComponent from "../IconicComponent";
 
 export default class Button extends IconicComponent {
+  static get observedAttributes() {
+    return ["iconic-style"];
+  }
+
   constructor() {
     super();
+
+    this.style.setProperty("--background", "var(--grey-light-1)");
+    this.style.setProperty("--background-light", "var(--grey-light-0)");
+    this.style.setProperty("--background-dark", "var(--grey-light-2)");
+    this.style.setProperty("--foreground", "var(--primary)");
+    this.style.setProperty("--foreground-highlight", "var(--primary-light)");
+
 
     this.css`
       .button-inner {
@@ -16,20 +27,20 @@ export default class Button extends IconicComponent {
         cursor: pointer;
         transition: 0.3s ease;
 
-        background: var(--primary);
-        box-shadow: inset 0.2rem 0.2rem 1rem var(--primary-light),
-          inset -0.2rem -0.2rem 1rem var(--primary-dark),
+        background: var(--background);
+        box-shadow: inset 0.2rem 0.2rem 1rem var(--background-light),
+          inset -0.2rem -0.2rem 1rem var(--background-dark),
           0.3rem 0.3rem 0.6rem var(--grey-light-2),
           -0.2rem -0.2rem 0.5rem var(--white);
-        color: var(--grey-light-1);
+        color: var(--foreground);
         text-transform: uppercase;
       }
       .button-inner:hover {
-        color: var(--white);
+        color: var(--foreground-highlight);
       }
       .button-inner:active {
-        box-shadow: inset 0.2rem 0.2rem 1rem var(--primary-dark),
-          inset -0.2rem -0.2rem 1rem var(--primary-light);
+        box-shadow: inset 0.2rem 0.2rem 1rem var(--background-dark),
+          inset -0.2rem -0.2rem 1rem var(--background-light);
       }
       button, button:hover, button:active, button:focus {
         border: 0;
@@ -46,6 +57,16 @@ export default class Button extends IconicComponent {
         <p class="button-inner">${this.innerHTML}</p>
       </button>
     `;
+  }
+  attributeChangedCallback(name: string, _: any, newValue: string) {
+    console.log(name, _, newValue);
+    if (name === "iconic-style") {
+      this.style.setProperty("--background", "var(--primary)");
+      this.style.setProperty("--background-light", "var(--primary-light)");
+      this.style.setProperty("--background-dark", "var(--primary-dark)");
+      this.style.setProperty("--foreground", "var(--grey-light-1)");
+      this.style.setProperty("--foreground-highlight", "var(--white)");
+    }
   }
 }
 
